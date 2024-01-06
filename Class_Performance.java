6e502633389d184b8d17faff4b55be08ad8b8b00
Sneha-Pulse <72751097+Sneha-Pulse@users.noreply.github.com>
2024-01-06 06:45:01 +0530

    Class_Performance.java

--- Class_Performance.java
+++ Class_Performance.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
+//Defining class
 public class Performance {
-    
+
+    //Declaring a private array of integers named marks to store the marks of 10 students
     private int[] marks;
 
-    
+    //This constructor initializes the marks array with a size of 10 when a Performance object is created
     public Performance() {
         marks = new int[10];
     }
 
+    //User can enter marks for 10 students and store them in the marks array
     public void readMarks() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter marks for 10 students:");
@@ -16,6 +19,7 @@ public class Performance {
             marks[i] = scanner.nextInt();
         }
     }
+    //Method to find and return the highest mark in the marks array
     public int highestMark() {
         int max = marks[0];
         for (int i = 1; i < marks.length; i++) {
@@ -26,6 +30,7 @@ public class Performance {
         return max;
     }
 
+    //Method to find and return the least mark in the marks array
     public int leastMark() {
         int min = marks[0];
         for (int i = 1; i < marks.length; i++) {
@@ -36,6 +41,7 @@ public class Performance {
         return min;
     }
 
+    //Method to find and return the mode in the marks array
     public int getMode() {
         int mode = marks[0];
         int maxFrequency = 1;
@@ -58,6 +64,8 @@ public class Performance {
 
         return mode;
     }
+
+    //Method to find and return the frequency of the mode in the marks array
     public int getFreqAtMode() {
         int mode = getMode();
         int frequency = 0;
@@ -70,6 +78,8 @@ public class Performance {
 
         return frequency;
     }
+
+    //Method to print the highest mark, least mark, mode, and mode frequency to the console
     public void display() {
         System.out.println("Highest Mark: " + highestMark());
         System.out.println("Least Mark: " + leastMark());
@@ -77,6 +87,7 @@ public class Performance {
         System.out.println("Mode Frequency: " + getFreqAtMode());
     }
 
+    //Creating a Performance object, calls the readMarks method to read marks from the user, and then calls the display method to print the results
     public static void main(String[] args) {
         Performance performance = new Performance();
         performance.readMarks();
